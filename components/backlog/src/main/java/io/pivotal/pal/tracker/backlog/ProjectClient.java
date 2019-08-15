1694f8e274af20997af23c57705c99473560c7c6
Sai Jithendra <saijithendra.kodavaluru@gmail.com>
2019-08-15 10:46:29 -0400

    circuit breaker

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,5 +1,7 @@
 package io.pivotal.pal.tracker.backlog;
 
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
@@ -12,6 +14,7 @@ public class ProjectClient {
         this.endpoint = registrationServerEndpoint;
     }
 
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
         return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
     }
